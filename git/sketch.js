4d7fca7c50f192cd74bea867f5924f226bb9fa0b
HrafnFlokiP <hrafnfloki@gmail.com>
2025-10-01 09:11:32 +0200

    Proggraming simon dk

---
+++ git/sketch.js
@@ -0,0 +1,15 @@
+const totalPages = 4;
+let currentPage = 1;
+
+function goToPage(pageNumber) {
+    for (let i = 1; i <= totalPages; i++) {
+        if (i == pageNumber) {
+            document.getElementById('page'+i).classList.add('show');
+        } else {
+            document.getElementById('page'+i).classList.remove('show');
+        }
+    }
+    currentPage = pageNumber;
+    console.log("Current Page", currentPage);
+    return currentPage
+}
