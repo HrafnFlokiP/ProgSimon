29a687ba43662c7b997ba533cb28092e67c8d3d0
HrafnFlokiP <hrafnfloki@gmail.com>
2025-10-05 18:29:13 +0200

    Verkefni

---
+++ git/cssGridWork/sketch.js
@@ -0,0 +1,16 @@
+const pagesC = 9;
+let CurrentP = 1;
+
+function goToPage(pageN){
+    for (let i = 1; i <= pagesC; i++ ){
+        if (i == pageN){
+            document.getElementById('page'+i).classList.add('show');
+        }  else {
+            document.getElementById('page'+i).classList.remove('show');
+        }
+    }
+    CurrentP = pageN
+    console.log('current page;',currentPage)
+    return CurrentP
+}
+
